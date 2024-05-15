29b86ae2752d7d6d7c6ff96c6d368f5e663ce7bf
Ilya Vasilyev <vasilyev.i123@gmail.com>
2024-05-15 20:30:24 +0300

    refactor: add "npx playwright install"

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ npm i -g wait-port@1.0.4 > /dev/null
 npm ci > /dev/null
 npm ci --prefix /tmp/tests-blog-customizer > /dev/null
 npx playwright install --with-deps > /dev/null
+npx playwright install
 
 echo "СБОРКА ПРОЕКТА"
 npm run build
