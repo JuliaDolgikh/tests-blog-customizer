5c854f36b0c959aec60cc3d02fe75f44c9cfb996
Ilya Vasilyev <vasilyev.i123@gmail.com>
2024-05-15 21:06:40 +0300

    fix run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,7 @@ echo "УСТАНОВКА ЗАВИСИМОСТЕЙ"
 npm i -g wait-port@1.0.4 > /dev/null
 npm ci > /dev/null
 npm ci --prefix /tmp/tests-blog-customizer > /dev/null
-npx playwright install --with-deps
-npx playwright install-deps
+npx playwright install --with-deps > /dev/null
 
 echo "СБОРКА ПРОЕКТА"
 npm run build
