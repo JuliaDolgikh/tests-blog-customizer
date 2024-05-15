90aa8a3d8b6d5a33d8640e817f2df20ad1ca81dc
Ilya Vasilyev <vasilyev.i123@gmail.com>
2024-05-15 20:51:56 +0300

    fix run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ npm i -g wait-port@1.0.4 > /dev/null
 npm ci > /dev/null
 npm ci --prefix /tmp/tests-blog-customizer > /dev/null
 npx playwright install > /dev/null
+npx playwright install-deps > /dev/null
 
 echo "СБОРКА ПРОЕКТА"
 npm run build
